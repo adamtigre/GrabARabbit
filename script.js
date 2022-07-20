933238b212095fac75df71cd4019bb9e6690bfd2
unknown <peteradamu19@yahoo.com>
2022-07-20 04:03:24 +0100

    coded javascrip

---
+++ script.js
@@ -0,0 +1,13 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+//create a function to make a random time for mole to pop from the hole
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+moles.forEach(mole => mole.addEventListener('click', wack))
+
